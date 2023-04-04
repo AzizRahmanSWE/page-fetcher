683d45a893f5147c69a08e173c66bcb7d31d2a2f
Friies <bahram.parsa2019@gmail.com>
2023-04-04 11:17:17 -0400

    fix minor errors & add a example case

--- fetcher.js
+++ fetcher.js
@@ -3,19 +3,25 @@ const fs = require('fs');
 
 
 const url = process.argv[2]; // takes in the url
-const fileDirectoryPath = process.arg[3]; // takes in the file path
+const fileDirectoryPath = process.argv[3]; // takes in the file path
 
-request(url, (error, response, body) => {
+// `request` function makes the HTTP request to the URL 
+request(url, (error, response, body) => { // function takes in three arugments as callback.
+
   if(error) {
     console.log(`Error: ${error}`)
   }
 
-  fs.writeFile(fileDirectoryPath, body, (error) => {
+  // use fs.writeFile function to write the body to the specific file directory path
+  fs.writeFile(fileDirectoryPath, body, (error) => { // cb fn to be executed once file is written.
+
     if (error) {
       console.log(`Error:  ${error}`);
       return;
     }
 
-    console.log(`The File ${body.length} bytes has been downloaded and saved to ${fileDirectoryPath}`);
+  // success msg once done downloading.
+    console.log(`Downloaded and saved ${body.length} bytes to ${fileDirectoryPath}`);
   });
 });
+
